5f8e327272c4ac9ffad11c9f18ff7457ce585e4c
Surojit Saha <sahasurojit06@gmail.com>
2022-10-11 20:54:12 +0530

    Footer removed

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -131,8 +131,8 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sumitproject1503@gmail.com'
-EMAIL_HOST_PASSWORD = 'sumityoyo'
+EMAIL_HOST_USER = 'osirisgamer56@gmail.com'
+EMAIL_HOST_PASSWORD = 'lyuvqgmjxphewzdg'
 
 # for sms
 SMS_BACKEND = 'sms.backends.console.SmsBackend'
